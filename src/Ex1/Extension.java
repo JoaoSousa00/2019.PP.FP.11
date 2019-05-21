7409135a0b816cd9477eda94da7f932cb183c34b
Joao Sousa <joao.men2@hotmail.com>
2019-05-21 13:52:59 +0100

    Second commit - No Demo & No Javadoc

---
+++ src/Ex1/Extension.java
@@ -0,0 +1,22 @@
+package Ex1;
+
+/**
+ * <p>
+ * <strong>Author: </strong><br>
+ * Joao Sousa<br>
+ * <strong>Description: </strong><br>
+ * Enumeração que
+ * </p>
+ */
+public enum Extension {
+        mp3, mp4, txt;
+        
+        public String MaterialToString(Extension extension){
+            switch(extension){
+                case mp3: return ".mp3";
+                case mp4: return ".mp4";
+                case txt: return ".txt";
+                default: return "Indefinido";
+            }
+        }
+}
